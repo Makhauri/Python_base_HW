4ba62bd958c5a2fb7b34f91411b388e7cf2a26fd
Makhauri <makhaurir@mail.ru>
2020-04-30 22:56:08 +0300

    Les_6_start

--- les5_hw_001.py
+++
@@ -1,7 +0,0 @@
-while True:
-    user_data = input("Введите данные")
-    if not user_data:
-        break
-    else:
-        with open('les5_hw_data_001.txt', 'a', encoding='UTF-8') as file:
-            file.write(f'{user_data}\n')
